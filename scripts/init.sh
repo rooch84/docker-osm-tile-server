c5079c5b8e714f462de5fd3bcbd541af712f0ef4
Chris Rooney <chris@rooch.net>
2018-09-28 10:47:39 +0100

    Bring Apache2 to foreground

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,14 @@
- /etc/init.d/postgresql start
-service apache2 start
+rm -f /usr/local/apache2/logs/httpd.pid
+
+
+/etc/init.d/postgresql start
 echo -n "Waiting for postgres to start."
 until `pg_isready -q`; do
   echo -n "."  
   sleep 1
 done
+service apache2 start
 su - renderaccount -c "renderd -f -c /usr/local/etc/renderd.conf"
+
+
+apachectl -DFOREGROUND
