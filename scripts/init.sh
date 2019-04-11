dfb6cad2a9952c5c08634ef89442003858512c0b
Chris Rooney <chris@rooch.net>
2019-04-11 14:28:50 +0100

    Starts Apache properly

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,17 @@
+# Clear PID files to fix apache restart error
 rm -f /usr/local/apache2/logs/httpd.pid
 
+# Clear PID files to fix mod_tile restart error
+rm -rf /tmp/*
 
 /etc/init.d/postgresql start
-echo -n "Waiting for postgres to start."
+echo -n "Waiting for postgres to start.\n"
 until `pg_isready -q`; do
   echo -n "."  
   sleep 1
 done
-service apache2 start
-su - renderaccount -c "renderd -f -c /usr/local/etc/renderd.conf"
-
-
-apachectl -DFOREGROUND
+echo -n "Starting rendered...\n"
+su - renderaccount -c "renderd -f -c /usr/local/etc/renderd.conf" &
+sleep 5
+echo -n "Starting apache...\n"
+apachectl -D FOREGROUND
